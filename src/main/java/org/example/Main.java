7c20f7afb1fe92a9be7e9ea81ea321827f7aeb30
Sarmad Rehman <sarmadr02@gmail.com>
2024-09-12 01:25:11 +0500

    hibernate-orm config but modules are deprecated and code contains 🐞

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.dao.StudentDao;
+import org.entities.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
+
+        Student student = new Student(148, "Swarn", "Banglore");
+        int result = studentDao.insert(student);
+        System.out.println(result);
     }
 }
